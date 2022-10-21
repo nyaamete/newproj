7ecf34e5c809ad375c32b85bee8feac7227337a6
nyaamete <munshindaeiruomaewa@gmail.com>
2022-10-21 16:50:12 +0800

    Initial commit

---
+++ newproj.py
@@ -0,0 +1,25 @@
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("When is your birth year? ")
+age = 2022 - int(birth_year)
+if age <18:
+    print("lol he's " + str(age) + " bozos still a minor L")
+if age >18:
+    print("old fuck you're " + str(age) + " gonna die soon RIPBOZO")
+
+first_number = input("First number? ")
+print(first_number + " + ? ")
+second_number = input("second number? ")
+sum = float(first_number) +  float(second_number)
+print("Your sum is " + str(sum) + ", 😊 have a nice day! ")
+
+
+
+
+
+
+
+
+
+
